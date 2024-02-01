9e7b6664daeb51458a5db106651046ff719f53a8
Sunny Chugh <10683796@ltilbom20401298.lntinfotech.com>
2024-02-01 17:37:41 +0530

    test pipeline

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -5,8 +5,8 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com/SunnyChugh99/banking_management_golang/util"
 	"github.com/stretchr/testify/require"
-	"github.com/techschool/simplebank/util"
 )
 
 func createRandomAccount(t *testing.T) Account{
